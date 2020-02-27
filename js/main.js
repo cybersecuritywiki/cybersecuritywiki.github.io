47f18e112b653fc722cf126dddea9ae67d877301
Leandro Leberon <55678928+leandroleberon@users.noreply.github.com>
2020-02-27 11:37:38 -0500

    mod

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 $( document ).ready(function() {
+
+    $("#user-settings").slideUp();
+
+
     $("#sidebar").hover(function(){
         $(this).css("width","300px");
     }, function(){
         $(this).css("width","100px")
     })
+
+    $("#navbar img").click(function(){
+        $("#user-settings").slideToggle();
+    })
+
+
 });
