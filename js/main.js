91142776fc5fe6b20ca7335993f5aea28ce7db38
Leandro Leberon <55678928+leandroleberon@users.noreply.github.com>
2020-02-27 18:08:33 -0500

    mod

--- js/main.js
+++ js/main.js
@@ -1,22 +1,31 @@
 $( document ).ready(function() {
 
     $("#user-settings").slideUp();
-
-
     
     $("#sidebar").hover(function(){
         $(this).css("width","250px");
         $("#sidebar-links li").css("opacity","1");
         $("#sidebar-icons").css("opacity","0");
+        setTimeout(function(){
+            $("#sidebar-icons").css("display","none");
+          }, 400);
     }, function(){
+        $("#sidebar-icons").css("display","block");
         $(this).css("width","100px");
         $("#sidebar-links li").css("opacity","0");
-        $("#sidebar-icons").css("opacity","1");
+        setTimeout(function(){
+            $("#sidebar-icons").css("opacity","1")
+          }, 100);
+    })
+
+    $("#sidebar-links li").hover(function(){
+        $(this).css("background-color","rgba(226, 226, 226, 0.11)")
+    }, function(){
+        $(this).css("background-color","")
     })
 
     $("#navbar img").click(function(){
         $("#user-settings").slideToggle("300");
-
     })
 
 });
