ba80101668a0b72761e5275e0cc38ccf5c2be247
Leandro Leberon <55678928+leandroleberon@users.noreply.github.com>
2020-02-27 17:07:51 -0500

    mod

--- js/main.js
+++ js/main.js
@@ -5,17 +5,17 @@ $( document ).ready(function() {
 
     
     $("#sidebar").hover(function(){
-        $(this).css("width","300px");
-        $("#sidebar a").fadeIn();
-        $("#sidebar i").fadeOut();
+        $(this).css("width","250px");
+        $("#sidebar-links li").css("opacity","1");
+        $("#sidebar-icons").css("opacity","0");
     }, function(){
         $(this).css("width","100px");
-        $("#sidebar a").fadeOut("fast");
-        $("#sidebar i").fadeIn("slow");
+        $("#sidebar-links li").css("opacity","0");
+        $("#sidebar-icons").css("opacity","1");
     })
 
     $("#navbar img").click(function(){
-        $("#user-settings").slideToggle();
+        $("#user-settings").slideToggle("300");
 
     })
 
